8c23f36f3dcea9266e27622408e7b62c9e36fb02
Rubén Corral <errepunto83@gmail.com>
2019-02-17 22:14:30 +0100

    cesar corregido

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -3,6 +3,9 @@ package tudelft.caesarshift;
 public class CaesarShiftCipher {
 
     public String CaesarShiftCipher(String message, int shift){
+        if (message == null) {
+            return null;
+        }
         StringBuilder sb = new StringBuilder();
         char currentChar;
         int length = message.length();
@@ -12,15 +15,19 @@ public class CaesarShiftCipher {
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
            
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
-                return "invalid";
-            } else if ((char) (currentChar + shift) > 'z') {
-                currentChar = (char) (currentChar - 26);
-            } else if ((char) (currentChar + shift) < 'a'){
-                currentChar = (char) (currentChar + 26);
+            //sb.append(currentChar);
+            if (currentChar == ' ') {
+                sb.append(' ');
+            } else {
+                if (currentChar > 'z' || currentChar < 'a') {
+                    return "invalid";
+                } else if ((char) (currentChar + shift) > 'z') {
+                    currentChar = (char) (currentChar - 26);
+                } else if ((char) (currentChar + shift) < 'a') {
+                    currentChar = (char) (currentChar + 26);
+                }
+                sb.append((char) (currentChar + shift));
             }
-            sb.append((char) (currentChar + shift));
         }
 
         return sb.toString();
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,36 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    private CaesarShiftCipher instance;
+
+    @BeforeEach
+    public void initialize() {
+        this.instance = new CaesarShiftCipher();
+    }
+
+    @Test
+    public void nullMessage() {
+        final String result = instance.CaesarShiftCipher(null, 1);
+        Assertions.assertEquals(null, result);
+    }
+
+    @Test
+    public void emptyMessage() {
+        final String result = instance.CaesarShiftCipher("", 5);
+        Assertions.assertEquals("", result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"abc,3,def", "abc,1,bcd", "abc,0,abc", "abc,-1,zab", "abc def,2,cde fgh", "abc def,26,abc def", "aBc,1,invalid"})
+    public void testValues(final String msg, final int shift, final String expected) {
+        final String result = instance.CaesarShiftCipher(msg, shift);
+
+        Assertions.assertEquals(expected, result);
+    }
 }
